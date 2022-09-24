367329a072c4e0e1757751c652103b4d4ac4373a
Przemyslaw <misiek440@gmail.com>
2022-09-24 11:45:59 +0200

    add touch on phone

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 document.addEventListener("DOMContentLoaded", function () {
   const sections = [...document.querySelectorAll("Section")];
-  const section = document.querySelector("Section");
 
   const menuItems = [...document.querySelectorAll(".aside_nav li")];
   const ulMenu = document.querySelector(".aside_nav");
-  const li1 = document.getElementById("1");
-  // changeSection(0);
   let index = 0;
-  console.log(section);
+  let fingerDropY = null;
+  let directionSwipe;
+  let isThrottled = false;
+  menuListener(0);
 
-  const rec = section.getBoundingClientRect();
-  console.log(rec);
+  function menuListener() {
+    for (const el of menuItems) {
+      el.addEventListener("click", (el) => {
+        liIndex = parseFloat(el.target.getAttribute("data-menu"));
+
+        changeSection(liIndex, el);
+      });
+    }
+  }
 
   document.addEventListener("mousewheel", (e) => {
     if (e.deltaY < 0) {
@@ -22,20 +29,46 @@ document.addEventListener("DOMContentLoaded", function () {
         index += 1;
       }
     }
-    console.log(index);
-    changeSection(index);
-  });
-
-  menuItems.forEach((el, index) => {
-    el.addEventListener("click", (el) => {
-      liIndex = parseFloat(el.target.getAttribute("data-menu"));
-
-      console.log(rec);
-      changeSection(liIndex);
-    });
+    setTimeout(changeSection(index), 500);
   });
 
   function changeSection(index) {
+    drawActiveMenuItem(index);
     sections[index].scrollIntoView({ behavior: "smooth" });
+  }
+
+  function drawActiveMenuItem(index) {
+    for (const i of menuItems) {
+      i.classList.remove("active");
+    }
+    menuItems[index].classList.add("active");
+  }
+
+  addEventListener("touchstart", (e) => {
+    fingerDropY = e.touches[0].clientY;
+  });
+
+  document.addEventListener("touchmove", (e) => {
+    if (!fingerDropY) return;
+    fingerSwipeY = e.touches[0].clientY;
+    directionSwipe = fingerDropY - fingerSwipeY;
+
+    directionSwipe = directionSwipe < 0 ? 1 : -1;
+    fingerDropY = null;
+    directionTouch(directionSwipe);
+  });
+
+  function directionTouch(direction) {
+    console.log(index);
+    if (direction > 0) {
+      if (index <= 0) {
+        index = 0;
+      } else index -= 1;
+    } else if (direction < 0) {
+      if (index < sections.length - 1) {
+        index += 1;
+      }
+    }
+    changeSection(index);
   }
 });
